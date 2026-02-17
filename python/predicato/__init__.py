66efd815140d856d6c108b81a8441df20fb5af2d
jcalifornia <josh@mederrata.com>
2026-02-17 10:41:39 -0500

    chore: bump version to 0.9.7

--- python/predicato/__init__.py
+++ python/predicato/__init__.py
@@ -25,7 +25,7 @@ Crawler Example (requires `pip install predicato[crawler]`):
     ...     )
 """
 
-__version__ = "0.1.0"
+__version__ = "0.9.7"
 
 from predicato.client import AsyncPredicatoClient, PredicatoClient
 from predicato.exceptions import (
